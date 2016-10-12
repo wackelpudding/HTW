79c58310d8461558856f47ba05556a7578490096
Ricardo Jimenez Tuero <ricardo.jimeneztuero@adviqo.com>
2016-10-12 17:50:43 +0200

    restructuring

--- src/PROG1/Hello_World.c
+++ src/PROG1/Hello_World.c
@@ -1,6 +1,6 @@
 /*
  ============================================================================
- Name        : HTW.c
+ Name        : Hello_World.c
  Author      : Ricardo Jimenez Tuero
  Version     :
  Copyright   : HTW B1IKT WS1617
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("Let's say.. Hello World!"); /* prints All hail t3h World! */
+	puts("Let's say.. Hello World!"); /* prints Let's say.. Hello World! */
 	return EXIT_SUCCESS;
 }
