f528f73aa65a86d225e33803d7f100f106bd99db
unknown <s0558882@F1-INF-F225-03.fb1.fhtw-berlin.de>
2016-10-18 12:22:15 +0200

    Test from git bash

---
+++ 161018_DrawL1/161018_DrawL1/161018_DrawL1.cpp
@@ -0,0 +1,12 @@
+// 161018_DrawL1.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("\n |\n |\n |\n\ |\n |\n\ |________\n");
+	return 0;
+}
+
--- 161018_Hello_B1IKT/161018_Hello_B1IKT/161018_Hello_B1IKT.cpp
+++ 161018_Hello_B1IKT/161018_Hello_B1IKT/161018_Hello_B1IKT.cpp
@@ -6,6 +6,7 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	puts("Hallo B1IKT!\n");
 	return 0;
 }
 
