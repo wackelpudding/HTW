f8a6407a9086b76566cc2a5691938cb07354b5f5
Ricardo Jimenez Tuero <ricardo.jimeneztuero@student.htw-berlin.de>
2016-10-18 12:31:46 +0200

    Add 161018_DrawL3

---
+++ 161018_DrawL3/161018_DrawL3/161018_DrawL3.cpp
@@ -0,0 +1,17 @@
+// 161018_DrawL3.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+
+
+void drawl1()
+{
+	printf("\n |\n |\n |\n\ |\n |\n\ |________\n\n");
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	drawl1();
+	return 0;
+}
+
