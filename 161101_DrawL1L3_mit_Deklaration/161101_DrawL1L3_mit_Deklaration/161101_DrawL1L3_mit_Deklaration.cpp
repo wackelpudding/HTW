b654ec3de286d770f0bc97324b1ae8a7de9d1b7f
Ricardo Jimenez Tuero <ricardo.jimeneztuero@student.htw-berlin.de>
2016-11-01 10:07:59 +0100

    DrawL1L3_mit_Deklaration added

---
+++ 161101_DrawL1L3_mit_Deklaration/161101_DrawL1L3_mit_Deklaration/161101_DrawL1L3_mit_Deklaration.cpp
@@ -0,0 +1,23 @@
+// 161101_DrawL1L3_mit_Deklaration.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+
+
+void drawl1()
+{
+	printf("\n |\n |\n |\n\ |\n |\n\ |________\n\n");
+}
+
+void drawl3()
+{
+	printf("  ________\n |\n |\n |\n\ |\n |\n\ |\n\n");
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	drawl1();
+	drawl3();
+	return 0;
+}
+
