de08b863f7c569a30f40305ef92dcb96e2758f15
Ricardo Jimenez Tuero <ricardo.jimeneztuero@student.htw-berlin.de>
2016-11-01 10:41:45 +0100

    added inch_in_mm

---
+++ 20161101_inch_in_mm/20161101_inch_in_mm/20161101_inch_in_mm.cpp
@@ -0,0 +1,38 @@
+/* 
+20161101_inch_in_mm.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+
+Aufgabe:
+- Wir wollen den Wert in Inch einlesen
+- Umrechnung von "in" in "mm"
+- Ergebnis der Umrechnung ausgeben
+
+Anforderungen:
+- Mitteilung seines Verwendudungszwecks
+- Dezimalwert für inch mit einer DNachkommastelle
+- Vorgabewert und Umrechnungswert solleb veide ausgegeben werden
+- Nach der Ausgabe des Umrechnungswertes wird das Programm beendet
+
+Formel:
+ - 1 Inch = 25.4 mm
+
+ Algorithmus:
+ #preprozessor (include std libs .. stdafx.h)
+ 1. Gebe den Zweck des Programms aus
+ 2. Lies die Größen in Inch ein
+ 3. Konvertiere Inch in mm mit Hilfe einer Variable according to formula
+ 4. Gebe die Ergebnisse aus 
+ 
+ 
+ */
+
+#include "stdafx.h"
+
+
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
---
+++ 20161101_inch_in_mm/20161101_inch_in_mm/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : Quelldatei, die nur die Standard-Includes einbindet.
+// 20161101_inch_in_mm.pch ist der vorkompilierte Header.
+// stdafx.obj enthält die vorkompilierten Typinformationen.
+
+#include "stdafx.h"
+
+// TODO: Auf zusätzliche Header verweisen, die in STDAFX.H
+// und nicht in dieser Datei erforderlich sind.
