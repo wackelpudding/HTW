d617909fc59782de7428b49f983bd1b6bdbf164d
Ricardo Jimenez Tuero <ricardo.jimeneztuero@adviqo.com>
2016-12-13 08:30:36 +0100

    2016121302 - Bug: "display wrong monthy rate at the end" fixed

--- KeditZins/KeditZins/KeditZins.cpp
+++ KeditZins/KeditZins/KeditZins.cpp
@@ -1,12 +1,13 @@
 // KeditZins.cpp
 // Ricardo Jimenez Tuero - B1IKT - Grundlagen der Programmierung
+//
 
-#include "stdafx.h""
+#include "stdafx.h"
 
 
 int Jahre;
 float KreditRahmen, ZinsSatz, ZinsSumme, Rate, Zinsen;
-float OrgKredit;
+float OrgKredit, OrgRate;
 
 float zinsrechnung(float KreditRahmen, float ZinsSatz){
 	return (KreditRahmen * ZinsSatz / 100);
@@ -17,29 +18,29 @@ float kreditrechnung(float KreditRahmen, float Zinsen, float Rate){
 }
 
 void DatenAufnahme(){
-	printf("Guten Tag,\n um ihnen Ihren Kreditzins und die Laufzeit zu berechnen\nbenoetige ich folgende Daten:\n");
+	printf("Guten Tag,\n um ihnen Ihren Kreditzins \nund die Laufzeit zu berechnen benoetige ich folgende Daten:\n");
 	printf("- Kreditrahmen\n- aktueller Zinssatz\n- Wunschrate\n\n");
 
 	// Aufnahme der Grundlegenden Daten die keiner tieferen Pruefung erfordern.
 	printf("Kreditrahmen in GELD (XXX.XX): ");
-	scanf_s("%f", &KreditRahmen);
+	scanf("%f", &KreditRahmen);
 	printf("\nZinssatz (XXX.XX%%): ");
-	scanf_s("%f", &ZinsSatz);
+	scanf("%f", &ZinsSatz);
 
 	Zinsen = zinsrechnung(KreditRahmen, ZinsSatz);
 	printf("\n\nAuf Grunder eingegebenen Daten ergibt sich jaehrliche Zinsen von: %.2f GELD\n\n", Zinsen);
-	//Die Raten d�rfen nicht kleiner als die Zinsen gew�hlt werden.. Kundenfreundlichkeit.. pah!
+	//Die Raten d�rfen nicht kleiner als die Zinsen gewaehlt werden.. Kundenfreundlichkeit.. pah!
 	do
 	{
 		printf("\nBitte geben Sie die Rate ein (groesser als Zinsen!): ");
-		scanf_s("%f", &Rate);
+		scanf("%f", &Rate);
 	} while (Rate <= Zinsen);
 }
 
 void Berechnung(){
 	OrgKredit = KreditRahmen; //Sicherung des Originalen Kreditrahmens
 	Jahre = 0;						//Alles hat einen Anfang..
-	while (KreditRahmen > 0){	//Somit ergibt sich, dass die letzte Rate bei nichterf�llen dieser Bedingung = Kreditrahmen ist.
+	while (KreditRahmen > 0){	//Somit ergibt sich, dass die letzte Rate bei nichterfuellen dieser Bedingung = Kreditrahmen ist.
 		Jahre++;
 		Zinsen = zinsrechnung(KreditRahmen, ZinsSatz);
 		KreditRahmen = kreditrechnung(KreditRahmen, Zinsen, Rate);
@@ -47,6 +48,7 @@ void Berechnung(){
 	}
 	if (KreditRahmen < 0)
 	{
+		OrgRate = Rate;
 		Rate = Rate + KreditRahmen;
 		printf("\nDie letzte Rate belaeuft sich auf: %.2f GELD", Rate);
 	}
@@ -55,7 +57,7 @@ void Berechnung(){
 
 void DatenAusgabe(){
 	printf("\n\nKreditrahmen: %9.2f GELD\n", OrgKredit);
-	printf("Monatliche Rate: %9.2f GELD\n", Rate);
+	printf("Monatliche Rate: %9.2f GELD\n", OrgRate);
 	printf("Gesamte Laufdauer: %9.2d Jahre\n", Jahre);
 	printf("Zinssumme: %9.2f GELD\n\n", ZinsSumme);
 }
@@ -72,7 +74,7 @@ void Start(){
 
 		//Abfrage, ob beendet werden soll.
 		printf("\nMoechten Sie das Programm erneut ausführen? Ja(1)/ Nein(0): ");
-		scanf_s("%d", &Choice);
+		scanf("%d", &Choice);
 		switch(Choice)
 			{
 			case 0:
