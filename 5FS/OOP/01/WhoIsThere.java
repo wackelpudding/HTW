4fa28373af391ab464212cc02e6a5c6a1d685fe6
Ricardo Jimenez Tuero <ricardo.jimeneztuero@adviqo.com>
2018-10-09 22:52:04 +0200

    OOP Uebung 1

---
+++ 5FS/OOP/01/WhoIsThere.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class WhoIsThere {
+   public static void main(String[] args) {
+     String name;
+
+     do {
+       System.out.println("Wer ist da?");
+       Scanner readInput = new Scanner(System.in);
+       name = readInput.nextLine();
+       if (!name.isEmpty()){
+         System.out.println("Hallo " + name + "!");
+       } else {
+         System.out.println("....Hallo?");
+        }
+      } while (name.isEmpty());
+   }
+}
