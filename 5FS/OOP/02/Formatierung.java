6c7e1ded5a5656bb35c26e63d0a77281acab6eaa
Ricardo Jimenez Tuero <ricardo.jimeneztuero@adviqo.com>
2018-10-17 21:24:06 +0200

    OOP remoastered

--- 5FS/OOP/02/Formatierung.java
+++ 5FS/OOP/02/Formatierung.java
@@ -14,31 +14,3 @@ class Formatierung {
 		}
 	}
 }
-
-
-
-/*
-a)
-In der Klasse CallByX wird folgendes ausgegbeben, beginnend mit Konsoleneingabe:
-#>java CallByX
-#>3
-#>3
-#>4
-#>3
-
-In der Klasse CallByY wird folgendes ausgegbeben, beginnend mit Konsoleneingabe:
-
-#>java CallByY
-#>3
-#>3
-#>4
-#>4
-
-b)
-CallByX ist Call-By-Value und CallByY ist Call-By-Reference.
-in Class 1 wird x mit 3 initialisiert und der Wert an increment übergeben.
-In increment wird x' ausgegeben, um 1 erhöht und nochmal ausgegeben.
-Anschließend wird das ursprüngliche, nicht editierte x ausgegeben.
-In Class 2 wird die Speicheradresse von x an inkrement übergeben.
-Somit wird bei der änderung mit inkrement auch der tatsächliche Wert von x geändert.
-*/
