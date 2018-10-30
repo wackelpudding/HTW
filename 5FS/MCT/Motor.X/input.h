dcfbdc2b9405b0f42d43b526374329490ad759e7
Ricardo Jimenez Tuero <ricardo.jimeneztuero@adviqo.com>
2018-10-30 10:41:10 +0100

    update

---
+++ 5FS/MCT/Motor.X/input.h
@@ -0,0 +1,46 @@
+/* 
+ * File:   input.h
+ * Author: Ricardo
+ *
+ * Created on June 17, 2018, 12:23 AM
+ */
+
+#ifndef INPUT_H
+#define	INPUT_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include "mcc_generated_files/mcc.h"
+
+
+/*** prototypes ***************************************************************/    
+//Fragt einen der 4 Inputs an und
+// liefert den aktuellen Zustand des Inputs zurück
+bool GetInput(int in, bool out0, bool out1, bool out2, bool out3); 
+
+//sendet einen string via EUSART_Write
+void send_string(const char *x);
+
+//Fragt den Temperatursensor ab und liefert die Tempteratur 
+//als integer ein ein Zehntel Grad genau. (222 entsprechen 22,5 grad celsius))
+int getTemp(void);
+
+
+extern unsigned char SENSOR[9];
+
+//deklaration der Input Variablen
+bool out0 = false;
+bool out1 = false;
+bool out2 = false;
+bool out3 = false;
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* INPUT_H */
+
---
+++ 5FS/MCT/Motor.X/main.c
@@ -0,0 +1,102 @@
+/**
+  Generated Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.65.2
+        Device            :  PIC16F1937
+        Driver Version    :  2.00
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#include "mcc_generated_files/mcc.h"
+
+
+/*
+                         Main application
+ */
+uint16_t convertedValue = 0;
+uint8_t percent =0;
+char Buffer[] = {"InitalizeValue"};
+char sint[3];
+
+void main(void)
+{
+    // initialize the device
+    SYSTEM_Initialize();
+
+    // When using interrupts, you need to set the Global and Peripheral Interrupt Enable bits
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    //INTERRUPT_GlobalInterruptEnable();
+
+    // Enable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+    
+    EPWM1_LoadDutyValue(1023);
+    IN1_SetHigh();
+    IN2_SetLow();
+
+    Lcd_Init(); //Initialisierung des LCD
+    //RW_SetLow(); // Read-Modus
+
+    Lcd_Clear(); // Bildschirm clearen
+    Lcd_Set_Cursor(1,1); //Cursor nach oben Links (1,1) setzen
+    Lcd_Write_String("Welfrieden:"); // Text in der oberen Zeile
+    Lcd_Set_Cursor(2,1);
+    Lcd_Write_String("Done:");
+    send_string("\033[H\033[J");
+    send_string("Weltfrieden\r\nDone:");
+        
+    while (1)
+    {
+        convertedValue = ADC_GetConversion(POT); //Konvertierte Wert in Variablen "convertedValue" speichern
+        EPWM1_LoadDutyValue(convertedValue);
+        percent = (uint8_t)( (float)convertedValue /1023*100); //Umrechnen in den Spannungswert
+        Lcd_Set_Cursor(2,6);
+        send_string("\033[2;6H");
+        send_string("...");
+        conv_int_to_string(percent);
+        send_string("%");
+        Lcd_Write_String("%");
+     }
+}
+/**
+ End of File
+ */
